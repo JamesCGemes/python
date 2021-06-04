4a03fbbd87c64bae690f1a4d641ae333b3697eb2
James Gemes <james.gemes@gmail.com>
2021-06-04 15:05:01 -0500

    learning operations with lists

--- basics_old.py
+++ basics_old.py
@@ -16,10 +16,31 @@
 studentGrades = {"James" : 80.0, "Cassie" : 95.5}
 #this shows the value of each item in a dict
 print(studentGrades.values())
-
+# this adds the values of each item in the dict
 gradesSum = sum(studentGrades.values())
 
-print(gradesSum)
+# print(gradesSum)
+
+myNumbers = [30, 40, 50, 60, 70]
+
+#adds new value to end of the list
+
+myNumbers.append(80)
+
+print(myNumbers)
+
+toClear = [10, 20, 30, 40]
+#clears the items in a list
+toClear.clear()
+
+print(toClear)
+
+numbers = [1,2,3,4,5,6]
+
+print(numbers.index(3))
+
+
+
 
 
 

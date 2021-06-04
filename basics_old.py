fad1e132e9186922209dc80e990266b80895e82d
James Gemes <james.gemes@gmail.com>
2021-06-04 14:25:31 -0500

    learning about dictionary types

--- basics_old.py
+++ basics_old.py
@@ -1,1 +1,25 @@
-print(24 * 7)
+# print(24 * 7)
+
+# student_grades = [9.1, 8.8,7.5,9.1]
+
+# gradesSum = sum(student_grades)
+# gradesLength = len(student_grades)
+
+# gradesMean = gradesSum / gradesLength
+
+# print(max(student_grades))
+
+# countGrades = student_grades.count(9.1)
+
+# print(countGrades)
+
+studentGrades = {"James" : 80.0, "Cassie" : 95.5}
+#this shows the value of each item in a dict
+print(studentGrades.values())
+
+gradesSum = sum(studentGrades.values())
+
+print(gradesSum)
+
+
+

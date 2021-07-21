be2b653ba0496e7ca9d71e8470fb0ea5b61285e3
James Gemes <james.gemes@gmail.com>
2021-07-21 12:57:32 -0500

    Working on user inputs

--- basics.py
+++ basics.py
@@ -32,14 +32,48 @@ def f_to_m(input):
 
 studentGrades = {"James" : 80.0, "Cassie" : 95.5, "Lee" :100}
 
-
+#below will take a list or dict
 def mean(value):
-    if type(value) == dict:
+    #this checks for the type of the input dict vs list
+    #if type(value) == dict:
+    if isinstance(value, dict):
         the_mean = sum(value.values()) / len(value)
     else:    
         the_mean = sum(value) / len(value)
     return the_mean
 
     
-print(mean(studentGrades))    
+#print(mean(studentGrades))    
 
+#function that prints true if string has more than eight characters or false if it has less.
+
+def has_eight(input):
+    if len(input) < 8:
+        return False
+    elif len(input) >= 8:
+        return True    
+
+#print(has_eight("jamesgemes"))
+
+def temp(input):
+    if input <= 7:
+        return "Cold"
+    else:
+        return "Warm"    
+
+#print(temp(7))
+
+def which_temp(input):
+    if input in range(15, 26):
+        return "Warm"
+    elif input < 15:
+        return "Cold"
+    else:
+        return "Hot"    
+
+
+#print(which_temp(-5))            
+
+#take user input for which_temp
+user_input = float(input("Please enter the temperature:"))
+print(which_temp(user_input))

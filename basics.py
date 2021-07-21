c7d9c328e7970f36a0e894a799e1c19b08d5be33
James Gemes <james.gemes@gmail.com>
2021-07-21 11:22:32 -0500

    created function that can handle a dict or list

--- basics.py
+++ basics.py
@@ -3,16 +3,16 @@ week_days = 7
 week_hours = day_hours * week_days
 #print("there are" , week_hours, " hours in a week")
 
-studentGrades = {"James" : 80.0, "Cassie" : 95.5, "Lee" :100}
+# studentGrades = {"James" : 80.0, "Cassie" : 95.5, "Lee" :100}
 
-#print(studentGrades["Cassie"])
+# #print(studentGrades["Cassie"])
 
-def mean(mylist):
-    the_mean = sum(mylist) / len(mylist)
-    return the_mean
+# def mean(mylist):
+#     the_mean = sum(mylist) / len(mylist)
+#     return the_mean
 
     
-print(mean([1,4,5]))    
+# print(mean([1,4,5]))    
 
 
 #function that calculates area of a square with even sides
@@ -21,10 +21,25 @@ def area_of_square(l):
     return area_of
 
 
-    
+ #convert fluid ounce to milli   
 def f_to_m(input):
     milli = 29.57353
     convert_to = input * milli
     return convert_to   
 
-print(f_to_m(5))    
+#print(f_to_m(5))    
+
+
+studentGrades = {"James" : 80.0, "Cassie" : 95.5, "Lee" :100}
+
+
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:    
+        the_mean = sum(value) / len(value)
+    return the_mean
+
+    
+print(mean(studentGrades))    
+

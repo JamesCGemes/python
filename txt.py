9b90e7f961ce0afaa80c8ce8eefc83c10227e67c
James Gemes <james.gemes@gmail.com>
2021-08-04 15:17:29 -0500

    writing text to a file

--- txt.py
+++ txt.py
@@ -1,3 +1,13 @@
 myfile = open('fruits.txt')
 
-print(myfile.read())
+# print(myfile.read())
+
+#better way to open and read a file
+
+with open("fruits.txt") as myfile:
+    content = myfile.read()
+
+
+print(content)    
+
+

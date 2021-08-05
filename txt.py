96384103d4df16433005687db8f7e17c417fc951
James Gemes <james.gemes@gmail.com>
2021-08-05 08:12:22 -0500

    write to file

--- txt.py
+++ txt.py
@@ -1,3 +1,6 @@
+
+
+
 myfile = open('fruits.txt')
 
 # print(myfile.read())
@@ -8,6 +11,22 @@ with open("fruits.txt") as myfile:
     content = myfile.read()
 
 
-print(content)    
+print(content)   
 
+with open("test/veggies.txt", "w") as veggie_names:
+    veggie_names.write("tomato")
+    veggie_names.write("\ncucumber")
+
+    
+
+
+# with open("fruits.txt") as fruit_file:
+#     content = fruit_file
+    
+    
+# print(content[:10]) 
+
+def how_many(character, path):
+    with open(path) as file_to_read:
+        content
 

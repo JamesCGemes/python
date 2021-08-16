4b381a34f0b32e9e54aebff933f9c7d7267de96c
James Gemes <james.gemes@gmail.com>
2021-08-16 15:45:22 -0500

    working on yes or no inputs

---
+++ app01.py
@@ -0,0 +1,25 @@
+import json
+from difflib import get_close_matches
+
+
+data = json.load(open("dictionary/data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches (w, data.keys())) > 0:  
+        yes_or_no = input("Did you mean %s instead? Enter Y or N: " % get_close_matches(w, data.keys())[0])
+        if yes_or_no == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yes_or_no == "N":
+            return "The word does not exist. Please check it"
+        else:
+            return "We didnt understand your entry"    
+    else: 
+        return "That word dosnt exist"
+        
+
+word = input("Enter a word: ")
+
+print(translate(word))

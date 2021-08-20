72d47b261dbcaefa848c2f86eff6e66e6968b466
James Gemes <james.gemes@gmail.com>
2021-08-20 15:50:21 -0500

    finished this one for now

--- app01.py
+++ app01.py
@@ -8,6 +8,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]    
     elif len(get_close_matches (w, data.keys())) > 0:  
         yes_or_no = input("Did you mean %s instead? Enter Y or N: " % get_close_matches(w, data.keys())[0])
         if yes_or_no == "Y":
@@ -22,4 +24,11 @@ def translate(w):
 
 word = input("Enter a word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+
+else:
+    print(output)       

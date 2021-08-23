b1de4db9d7584aaace20f30e68034214a3fc6c03
James Gemes <james.gemes@gmail.com>
2021-08-23 16:15:24 -0500

    creating dictionary that connects to a database

---
+++ app02.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
+
+cursor = con.cursor()
+
+query = cursor.execute("SELECT * FROM Dictionary")
+results = cursor.fetchall()
+
+print(results)
